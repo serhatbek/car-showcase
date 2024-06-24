3a06740cb4b9ed2c7882086bbc2df20a7278a865
serhatbek <serhat.bekk@gmail.com>
2024-06-24 14:54:37 +0300

    header

--- components/index.tsx
+++ components/index.tsx
@@ -1,2 +1,5 @@
 export { default as Hero } from './Hero/Hero';
 export { default as CustomButton } from './CustomButton/CustomButton';
+
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Footer } from './Footer/Footer';
--- types/index.ts
+++ types/index.ts
@@ -2,6 +2,7 @@ import { MouseEventHandler } from 'react';
 
 export interface CustomButtonProps {
   title: string;
+  btnType?: 'submit' | 'button';
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
